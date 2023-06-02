f4d87ae72c60bafd848356797110fbc4978193f4
AnonimoMC <messina.michele@iisgalvanimi.edu.it>
2023-06-02 12:03:38 +0000

    febuc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post/post.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01_Angular_empty';
+  title = 'facebook-site';
+  posts: Post[]
+
+  error_appear: boolean
+
+  post_height: number;
+
+  constructor(){
+    this.posts = []
+    this.posts = [
+      new Post('Giovanni', 'Daje Roma'),
+      new Post('Matteo', 'Wow'),
+      new Post('Marco', 'Ehi')
+    ]
+
+    this.error_appear = false
+    this.post_height = 470
+  }
+  sorted_posts(): Post[]{
+    return this.posts.sort((a: Post, b: Post) => (b.like - a.like))
+  }
+
+  submit_add_post(autore: HTMLInputElement, testo: HTMLTextAreaElement) {
+    if (autore.value != '' && testo.value != ''){
+      this.post_height = 470
+      this.error_appear = false
+
+
+      this.posts.push(new Post(autore.value, testo.value))
+      autore.value = ''
+      testo.value = ''
+    }else{
+      this.error_appear = true
+      this.post_height = 510
+
+    }
+    
+  }
 }
